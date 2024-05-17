e5439c840d0d3090c9d0bc2fb173a1c3ae0e2f73
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-05-18 00:07:13 +0600

    Create Toggle_Switch.cpp

---
+++ Toggle_Switch.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define int long long
+#define float double
+#define endl '\n'
+using namespace std;
+/* 
+When I wrote this code ,
+only Allah and I knew what I did.
+   
+Now... only Allah knows
+*/
+
+int32_t main () 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;while(t--) 
+    {
+        int n;
+        cin>>n;
+        int x = sqrt(n);
+        cout<<x<<endl;
+    }
+    return 0;
+}
